bf70abc048763bcd5fca722d575558d5848e915f
IT <it@anugroup.net>
2019-09-20 14:50:10 +0530

    First Commit By CSK

---
+++ TimeTakenToCreatePTandCarbideEnquiry/TimeTakenToCreatePTandCarbideEnquiry.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="TimeTakenToCreatePTandCarbideEnquiry.aspx.cs" Inherits="TimeTakenToCreatePTandCarbideEnquiry.TimeTakenToCreatePTandCarbideEnquiry" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <center>
+        <div style="width: 800px; font-family: 'Helvetica Neue', 'Lucida Grande', 'Segoe UI', Arial, Helvetica, Verdana, sans-serif;
+            border: 2px solid black;">
+            <asp:Label ID="Label1" runat="server" Style="text-align: center; font-weight: 700;
+                font-size: xx-large; color: #000000;" Text="Time Taken To Create PT"></asp:Label></div>
+                
+        <br />
+        <table>
+        <tr>
+        <td>
+            <asp:RadioButton ID="rdbtn1" GroupName="unit" runat="server" Text="AWS-1" 
+                style="font-weight: 700; font-size: small; color: #000000;"/></td>
+            <td>
+                <asp:RadioButton ID="rdbtn2" GroupName="unit" runat="server" Text="AWS-2" 
+                    style="font-weight: 700; font-size: small; color: #000000;"/>
+            </td>
+            <td>
+                <asp:RadioButton ID="RadioButton1" GroupName="unit" Text="Both" runat="server" 
+                    style="font-weight: 700; font-size: small; color: #000000;" />
+            </td>
+        </tr>
+        </table><br />
+        <table>
+            <tr>
+                <td>
+                    <asp:Label ID="Label2" runat="server" Text="From : " 
+                        Style="font-weight: 700; font-size: medium; color: #000000;"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBox1" runat="server" Height="25px" Style="font-weight: 700;
+                        font-size: medium; text-align: center" Width="165px"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:Label ID="Label3" runat="server" Text="To : " 
+                        Style="font-weight: 700; font-size: medium; color: #000000;"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBox2" runat="server" Height="25px" Style="font-weight: 700;
+                        font-size: medium; text-align: center" Width="165px"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:Button ID="btnSubmit" runat="server" Text="Submit"
+                        Style="font-weight: 700; font-size: medium" onclick="btnSubmit_Click"/>
+                </td>
+            </tr>
+        </table><br />
+        <asp:Label ID="Label4" runat="server" Text="Label"></asp:Label>
+        <br />
+    <asp:GridView ID="GridView1" runat="server" CellSpacing="2" Style="color: Black;
+                text-align: center; height: 218px; width: 800px; color: Black; text-align: center;"
+                AutoGenerateColumns="False" ShowFooter="True" ShowHeaderWhenEmpty="True" >
+            <Columns>
+            <asp:TemplateField HeaderText="Sr No.">
+                        <ItemTemplate>
+                            <%#Container.DataItemIndex+1 %>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:BoundField DataField="OC" HeaderText="OC" ReadOnly="True" />
+                    <asp:BoundField DataField="Order_Date" HeaderText="Order Date" DataFormatString="{0:dd/MM/yyyy}" ReadOnly="True" />
+                    <asp:BoundField DataField="Customer_Code" HeaderText="Customer Code" ReadOnly="True" />
+                    <asp:BoundField DataField="Cust_PO" HeaderText="Customer PO" ReadOnly="True" />
+                    <asp:BoundField DataField="PODate" HeaderText="PO Date" DataFormatString="{0:dd/MM/yyyy}" ReadOnly="True" />
+                    <asp:BoundField DataField="DaystoMakePT" HeaderText="Days to Make PT" ReadOnly="True" />
+                    <asp:BoundField DataField="Location" HeaderText="Location" ReadOnly="True" />
+            </Columns>
+            <HeaderStyle BackColor="#ffffff" BorderStyle="Groove" />
+                <EmptyDataTemplate>
+                    No Record Available</EmptyDataTemplate>
+                <RowStyle BackColor="#EFF3FB" HorizontalAlign="Center" />
+                <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+                <AlternatingRowStyle BackColor="White" />
+            </asp:GridView><br />
+        </center>
+</asp:Content>
---
+++ TimeTakenToCreatePTandCarbideEnquiry/TimeTakenToCreatePTandCarbideEnquiry.aspx.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
+using System.Globalization;
+
+namespace TimeTakenToCreatePTandCarbideEnquiry
+{
+    public partial class TimeTakenToCreatePTandCarbideEnquiry : System.Web.UI.Page
+    {
+        OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=W:\test\Access\Tablas.mdb");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text != "" || TextBox2.Text != "")
+            {
+                PendingOC2();
+            }
+            else
+            {
+                string script = "alert('Please Enter From Date, To Date. !!!')";
+                System.Web.UI.ScriptManager.RegisterClientScriptBlock(btnSubmit, this.GetType(), "Test", script, true);
+            }
+        }
+        void PendingOC2()
+        {
+            //con.Open();
+            OleDbCommand cmd = new OleDbCommand();
+            string query;
+            cmd.Connection = con;
+            if (rdbtn1.Checked == true)
+            {
+                query = "Select NumPed AS OC, FecPed AS Order_Date, CliPed AS Customer_Code, PedPed AS Cust_PO, Location FROM [Pedidos de clientes] Where (FecPed Between format(#" + TextBox1.Text + "#, \"dd/MM/yyyy\") And format(#" + TextBox2.Text + "#, \"dd/MM/yyyy\")) AND (Location = 1)";
+            }
+            else if (rdbtn2.Checked == true)
+            {
+                query = "Select NumPed AS OC, FecPed AS Order_Date, CliPed AS Customer_Code, PedPed AS Cust_PO, Location FROM [Pedidos de clientes] Where (FecPed Between format(#" + TextBox1.Text + "#, \"dd/MM/yyyy\") And format(#" + TextBox2.Text + "#, \"dd/MM/yyyy\")) AND (Location = 2)";
+            }
+            else
+            {
+                query = "Select NumPed AS OC, FecPed AS Order_Date, CliPed AS Customer_Code, PedPed AS Cust_PO, Location FROM [Pedidos de clientes] Where FecPed Between format(#" + TextBox1.Text + "#, \"dd/MM/yyyy\") And format(#" + TextBox2.Text + "#, \"dd/MM/yyyy\")";
+            }
+            //string query = "Select NumPed AS OC, FecPed AS Order_Date, CliPed AS Customer_Code, PedPed AS Cust_PO, Location FROM [Pedidos de clientes] Where OcDate is Null";
+            OleDbDataAdapter ad = new OleDbDataAdapter(query, con);
+            DataTable dtpending = new DataTable();
+            ad.Fill(dtpending);
+            //con.Close();
+            dtpending.Columns.Add("Remarks", typeof(string));
+            dtpending.Columns.Add("PODate", typeof(DateTime));
+            dtpending.Columns.Add("DaystoMakePT", typeof(int));
+            dtpending.Columns["Location"].SetOrdinal(7);
+
+            dtpending.AcceptChanges();
+
+            foreach (DataRow dr in dtpending.Rows)
+            {
+
+                if (Convert.ToInt32(dr[0]) == 0)
+                {
+                    dr.Delete();
+
+                }
+                else
+                {
+                    string date = dr["Cust_PO"].ToString().Substring(dr["Cust_PO"].ToString().LastIndexOf(' ') + 1);
+                    //DateTime date2 = date.ToString(//DateTime.ParseExact(date, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+                    string shortDate = date;
+                    DateTime date3;
+                    if (date.Length != 0)
+                    {
+                        DateTime date4 = Convert.ToDateTime(date);
+                        string date5 = date4.ToShortDateString();
+                        date3 = Convert.ToDateTime(date5);
+                    }
+                    else
+                        date3 = Convert.ToDateTime(DateTime.Today.ToShortDateString());
+                    dr["PODate"] = date3.ToShortDateString();
+                    dr["DaystoMakePT"] = (Convert.ToDateTime(dr["Order_Date"]) - date3).TotalDays;
+                }
+
+            }
+
+            dtpending.DefaultView.Sort = "PODate asc";
+            GridView1.DataSource = dtpending;
+            GridView1.DataBind();
+            DataTable dtpricezero = new DataTable();
+            Label4.Text = dtpending.Rows.Count.ToString();
+        }
+        void CarbideInquiry()
+        {
+            OleDbCommand cmd = new OleDbCommand();
+            string query;
+            cmd.Connection = con;
+            if (rdbtn1.Checked == true)
+            {
+                 query = "Select NumPed AS OC, FecPed AS Order_Date, CliPed AS Customer_Code, PedPed AS Cust_PO, Location FROM [Pedidos de clientes] Where FecPed Between (format(#" + TextBox1.Text + "#, \"dd/mm/yyyy\") And format(#" + TextBox2.Text + "#, \"dd/mm/yyyy\")) AND Location = 1";
+            }
+            else if (rdbtn2.Checked == true)
+            {
+                 query = "Select NumPed AS OC, FecPed AS Order_Date, CliPed AS Customer_Code, PedPed AS Cust_PO, Location FROM [Pedidos de clientes] Where FecPed Between (format(#" + TextBox1.Text + "#, \"dd/mm/yyyy\") And format(#" + TextBox2.Text + "#, \"dd/mm/yyyy\")) AND Location = 2";
+            }
+            else
+            {
+                 query = "Select NumPed AS OC, FecPed AS Order_Date, CliPed AS Customer_Code, PedPed AS Cust_PO, Location FROM [Pedidos de clientes] Where FecPed Between format(#" + TextBox1.Text + "#, \"dd/mm/yyyy\") And format(#" + TextBox2.Text + "#, \"dd/mm/yyyy\")";
+            }
+            //string query = "Select NumPed AS OC, FecPed AS Order_Date, CliPed AS Customer_Code, PedPed AS Cust_PO, Location FROM [Pedidos de clientes] Where OcDate is Null";
+            OleDbDataAdapter ad = new OleDbDataAdapter(query, con);
+            DataTable dtCarbideInquiry = new DataTable();
+            ad.Fill(dtCarbideInquiry);
+        }
+    }
+}
